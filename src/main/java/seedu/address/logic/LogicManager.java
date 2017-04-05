165ba3c4eace77d22b68d5639ae6c7e3014afab3
Zhiyuan <jameslaizy@gmail.com>
2017-04-06 03:54:11 +0800

    Add subscriber to handle check box selection change event raised

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -2,9 +2,12 @@ package seedu.address.logic;
 
 import java.util.logging.Logger;
 
+import com.google.common.eventbus.Subscribe;
+
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.ComponentManager;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.commons.events.ui.CheckBoxSelectionChangedEvent;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -49,5 +52,23 @@ public class LogicManager extends ComponentManager implements Logic {
     @Override
     public ObservableList<ReadOnlyTask> getFilteredIncompleteTaskList() {
         return model.getFilteredIncompleteTaskList();
+    }
+
+    @Subscribe
+    private void handleCheckBoxSelectionChangedEvent(CheckBoxSelectionChangedEvent event) {
+        logger.info(LogsCenter.getEventHandlingLogMessage(event));
+        String status = "completed";
+        if (!event.getTaskStatus()) {
+            status = "incomplete";
+        }
+        String commandText = "mark " + event.getTaskIndex() + " " + status;
+        Command command = parser.parseCommand(commandText);
+        command.setData(model);
+        command.setDateParser(dtParser);
+        try {
+            command.execute();
+        } catch (CommandException e) {
+            e.printStackTrace();
+        }
     }
 }
